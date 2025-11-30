df735d1b0bc14c45a6a474a59f4dc46969983e38
DepMarketing <marketing@peabenguela.com>
2025-11-30 20:57:15 +0100

    feat: add initial sign up page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import HomePage from './pages/home'
+import LoginPage from './pages/login'
+import SignupPage from './pages/signup'
+import NotFoundPage from './pages/not-found'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
-);
+)
